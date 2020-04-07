7183ee3b975fab2b671d229836efa5c46e6209f4
elazar cohen <escohen115@gmail.com>
2020-04-07 18:07:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -10,8 +10,8 @@ def add_element_to_end_of_array(abc, f)
   abc.push(f)
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def add_element_to_start_of_array(abc, zero)
+  abc.unshift()
 end
 
 def remove_element_from_end_of_array(array)
